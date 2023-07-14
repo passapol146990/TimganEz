1ff1cdac6fac6db004ab782e5cb7f166ea7ec75b
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-07-14 16:12:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = require('./router/router')
+const router = require('/router/router')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
