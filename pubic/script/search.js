f32fec2e22d18942cce8db641de26b9a1c4f2f5b
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-07-29 22:52:02 +0700

    2

--- pubic/script/search.js
+++ pubic/script/search.js
@@ -1,5 +1,18 @@
 const search = document.getElementById('search');
+const div_item = document.getElementById('div_item');
+const div = div_item.querySelectorAll('div')
 search.addEventListener('input',(e)=>{
     e.preventDefault();
-    console.log(search.value)
+    let value = search.value
+    // if(value.length > 0){
+        div.forEach((item)=>{
+            if(item.id.includes(value)){
+                div_item.insertBefore(item,div_item.firstChild)
+            }else{
+                try{
+                    div_item.removeChild(item)   
+                } catch{}
+            }
+        })
+    // }
 })
